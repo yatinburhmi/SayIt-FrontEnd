c79b24cd97c4efc1630333565a3aab7385c7ccfa
yatinburhmi <yatinburhmi@gmail.com>
2022-08-02 10:44:07 -0400

    Added: createpost functionality

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -10,7 +10,14 @@ export const getPosts = () => async (dispatch) => {
         dispatch(action);
     }catch(error){
         console.log(error.message);
-    }
-    
-    
+    }    
+}
+
+export const createPost = (post) => async (dispatch) => {
+    try {
+        const {data} = await api.createPost(post);
+        dispatch({type: 'CREATE', payload:data})
+    }catch(error){
+        console.log(error.message);
+    }  
 }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,6 +1,9 @@
 import React, {useState} from "react";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import FileBase from 'react-file-base64';
+import { useDispatch } from "react-redux";
+import { createPost } from "../../actions/posts";
+
 import useStyles from "./styles";
 
 const Form = () => {
@@ -14,7 +17,13 @@ const Form = () => {
     })
   const classes = useStyles();
 
-  const handleSubmit = () => {};
+    const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createPost(postData))
+  };
+  
   const clear = () => {};
   return (
     <Paper className={classes.paper}>
