48028a4f165a60f9a54b1c211b59e88bedfccecc
yatinburhmi <yatinburhmi@gmail.com>
2022-08-02 10:25:09 -0400

    Added Modal for Create Post

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, {useEffect} from "react";
-import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import React, {useEffect, useState} from "react";
+import { Container, AppBar, Typography, Grow, Grid, Button, Modal, Box} from '@material-ui/core';
 import { useDispatch } from "react-redux";
 
 import { getPosts} from './actions/posts';
@@ -11,25 +11,38 @@ import useStyles from './styles';
 const App = () => {
     const classes = useStyles();
     const dispatch = useDispatch();
+    const [open, setOpenModal] = useState(false)
 
     useEffect(() => {
         dispatch(getPosts());
     }, [dispatch])
+
+    const handleOpenModal = () => {
+        setOpenModal(true)
+    }
+    const handleCloseModal = () => {
+        setOpenModal(false)
+    }
     return(
         <Container maxWidth="lg">
             <AppBar className={classes.appBar} position="static" color="inherit">
                 <Typography className={classes.heading} variant="h2" align="center">Say It!</Typography>
                 <img className={classes.image} src={sayIt} alt="facebook" height={60} /> 
+                <Button onClick={handleOpenModal} type="primary">Create Post</Button>
             </AppBar>
             <Grow in>
                 <Container>
+                    <Modal
+                        open={open}
+                        onClose={handleCloseModal}>
+                            <Box className={classes.modal}>
+                            <Form/>
+                            </Box>
+                        </Modal>
                 <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
-                    <Grid item xs={12} sm={7}>
+                    <Grid item xs={12} sm={12}>
                         <Posts />
                     </Grid>
-                    <Grid item xs={12} sm={4}>
-                        <Form/>
-                        </Grid>
                 </Grid>
                 </Container>
             </Grow>
--- src/styles.js
+++ src/styles.js
@@ -15,4 +15,15 @@ export default makeStyles(() => ({
   image: {
     marginLeft: '15px',
   },
+  modal:{
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    backgroundColor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  }
 }));
