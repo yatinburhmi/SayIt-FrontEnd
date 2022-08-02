10ff33ec3e2486e387d9e8fbb802e133160775a0
yatinburhmi <yatinburhmi@gmail.com>
2022-08-02 00:53:54 -0400

    Intitial UI setup

---
+++ src/components/Form/Form.js
@@ -0,0 +1,15 @@
+import React from "react";
+import useStyles from './styles';
+
+
+
+const Form = () => {
+    const classes = useStyles();
+    return(
+        <div>
+            <h1>Form</h1>
+        </div>
+    )
+}
+
+export default Form;
